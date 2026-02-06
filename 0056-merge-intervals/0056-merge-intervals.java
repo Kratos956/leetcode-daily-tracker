aae81261cbe362fe9312cd857385155b691d3b45
Sanit Chhonker <141418215+Kratos956@users.noreply.github.com>
2026-02-06 15:54:45 +0530

    Time: 8 ms (90.15%), Space: 49.1 MB (52.88%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,16 +1,25 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
-
+        if(intervals.length==0 || intervals.length==1) return intervals;
+        Arrays.sort(intervals,(a,b)->Integer.compare(a[0],b[0]));
         List<int[]> merged = new ArrayList<>();
-        for(int[] interval:intervals){
-            if(merged.isEmpty() || merged.get(merged.size()-1)[1]<interval[0]){
-                merged.add(interval);
+        int s1=intervals[0][0];
+        int e1=intervals[0][1];
+        for(int i=1;i<intervals.length;i++){
+            int s2=intervals[i][0];
+            int e2=intervals[i][1];
+            if(e1>=s2){
+                s1=s1;
+                e1=Math.max(e1,e2);
+                continue;
             }
-            else{
-                merged.get(merged.size()-1)[1]=Math.max(merged.get(merged.size()-1)[1],interval[1]);
-            }
+                merged.add(new int[]{s1,e1});
+                s1=s2;
+                e1=e2;
+                
         }
+        merged.add(new int[]{s1,e1});
+
         return merged.toArray(new int[merged.size()][]);
 
     }
