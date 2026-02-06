3caa1169b8ad02eabb5625650b90348d1d3b95bd
Sanit Chhonker <141418215+Kratos956@users.noreply.github.com>
2026-02-06 22:30:55 +0530

    Time: 8 ms (4.62%), Space: 47.1 MB (71.63%) - LeetHub

---
+++ 0057-insert-interval/0057-insert-interval.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        if(intervals.length==0) return new int[][]{newInterval};
+        List<int[]> list = new ArrayList<>();
+        boolean inserted=false;
+        for(int i=0;i<intervals.length;i++){
+            if(intervals[i][0]>=newInterval[0]){
+                list.add(new int[]{newInterval[0],newInterval[1]});
+                inserted=true;
+            }
+            list.add(new int[]{intervals[i][0],intervals[i][1]});
+        }
+        if(!inserted){
+            list.add(new int[]{newInterval[0],newInterval[1]});
+        }
+        return merge(list.toArray(new int[list.size()][]));
+
+
+
+    }
+    public int[][] merge(int[][] intervals) {
+        if(intervals.length==0 || intervals.length==1) return intervals;
+        Arrays.sort(intervals,(a,b)->Integer.compare(a[0],b[0]));
+        List<int[]> merged = new ArrayList<>();
+        int s1=intervals[0][0];
+        int e1=intervals[0][1];
+        for(int i=1;i<intervals.length;i++){
+            int s2=intervals[i][0];
+            int e2=intervals[i][1];
+            if(e1>=s2){
+                s1=s1;
+                e1=Math.max(e1,e2);
+                continue;
+            }
+            merged.add(new int[]{s1,e1});
+            s1=s2;
+            e1=e2;
+
+        }
+        merged.add(new int[]{s1,e1});
+
+        return merged.toArray(new int[merged.size()][]);
+
+    }
+}
