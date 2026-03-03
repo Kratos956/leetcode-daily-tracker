5354f99217dfadff1621e4d1df49618d90be58fa
Sanit Chhonker <141418215+Kratos956@users.noreply.github.com>
2026-03-03 14:52:59 +0530

    Time: 31 ms (51.35%), Space: 53.5 MB (88.06%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -1,26 +1,13 @@
-class Pair{
-    int[] first;
-    int second;
-    Pair(int[] a,int b){
-        first=a;
-        second=b;
-    }
-}
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        Map<int[], Integer> map = new HashMap<>();
-        for (int[] point : points) {
-            int x = point[0];
-            int y = point[1];
-            map.put(point, ( x * x  +  y * y));
-        }
-        PriorityQueue<Pair> pq = new PriorityQueue<>(
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>(
                 (a,b)->{
-                    return Math.toIntExact((b.second - a.second));
+                    return b[0]*b[0]+b[1]*b[1]-a[0]*a[0]-a[1]*a[1];
                 }
         );
-        for(Map.Entry<int[],Integer> entry : map.entrySet()){
-            pq.add(new Pair(entry.getKey(),entry.getValue()));
+        for(int[] point:points){
+            pq.add(point);
             if(pq.size()>k){
                 pq.poll();
             }
@@ -28,8 +15,7 @@ class Solution {
         int[][] ans = new int[k][2];
         int index = 0;
         while(!pq.isEmpty()){
-            Pair p = pq.poll();
-            ans[index++]=p.first;
+            ans[index++]=pq.poll();
         }
         return ans;
 
