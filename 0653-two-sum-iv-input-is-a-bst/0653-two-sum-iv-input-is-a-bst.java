1f81de6dc239b6fb2fb5ea23a671a86567c00771
Sanit Chhonker <141418215+Kratos956@users.noreply.github.com>
2026-03-17 19:46:51 +0530

    Time: 1 ms (99.98%), Space: 47.2 MB (66.64%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.java
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.java
@@ -1,21 +1,15 @@
 class Solution {
     public boolean findTarget(TreeNode root, int k) {
-        return dfs(root, root, k);
+        return dfs(root,root,k);
     }
-
-    boolean dfs(TreeNode node, TreeNode root, int k) {
-        if(node == null) return false;
-
-        int target = k - node.val;
-
-        // avoid using same node
-        if(target != node.val && find(root, target)) {
+    boolean dfs(TreeNode node,TreeNode root,int k){
+        if(node==null) return false;
+        int target=k-node.val;
+        if(target!=node.val && find(root,target)){
             return true;
         }
-
-        return dfs(node.left, root, k) || dfs(node.right, root, k);
+        return dfs(node.left,root,k) || dfs(node.right,root,k);
     }
-
     boolean find(TreeNode root, int k) {
         if(root == null) return false;
         if(root.val == k) return true;
