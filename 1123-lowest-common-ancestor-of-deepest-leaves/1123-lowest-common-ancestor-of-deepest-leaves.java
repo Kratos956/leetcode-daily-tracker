d7846d943e4da32b8de0b86c7327050922202675
Sanit Chhonker <141418215+Kratos956@users.noreply.github.com>
2026-03-19 16:43:23 +0530

    Time: 0 ms (100%), Space: 46.5 MB (39.63%) - LeetHub

--- 1123-lowest-common-ancestor-of-deepest-leaves/1123-lowest-common-ancestor-of-deepest-leaves.java
+++ 1123-lowest-common-ancestor-of-deepest-leaves/1123-lowest-common-ancestor-of-deepest-leaves.java
@@ -1,39 +1,30 @@
-class Solution {
-
-    class Pair {
-        TreeNode node;
-        int depth;
-
-        Pair(TreeNode node, int depth) {
-            this.node = node;
-            this.depth = depth;
-        }
+class Pair{
+    TreeNode node;
+    int depth;
+    Pair(TreeNode node,int depth){
+        this.node=node;
+        this.depth=depth;
     }
-
+}
+class Solution {
     public TreeNode lcaDeepestLeaves(TreeNode root) {
         return dfs(root).node;
     }
-
-    private Pair dfs(TreeNode root) {
-        // base case
-        if (root == null) {
-            return new Pair(null, 0);
+    Pair dfs(TreeNode root){
+        if(root==null){
+            return new Pair(null,0);
         }
-
-        Pair left = dfs(root.left);
-        Pair right = dfs(root.right);
-
-        // Case 1: left deeper
-        if (left.depth > right.depth) {
-            return new Pair(left.node, left.depth + 1);
+        Pair left=dfs(root.left);
+        Pair right=dfs(root.right);
+        
+        if(left.depth<right.depth){
+            return new Pair(right.node,right.depth+1);
         }
-
-        // Case 2: right deeper
-        if (right.depth > left.depth) {
-            return new Pair(right.node, right.depth + 1);
+        if(left.depth>right.depth){
+            return new Pair(left.node,left.depth+1);
+            
         }
-
-        // Case 3: equal depth → current node is LCA
-        return new Pair(root, left.depth + 1);
+        return new Pair(root,left.depth+1);
     }
+
 }
