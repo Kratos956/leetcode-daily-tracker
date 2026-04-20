7a680b14106165ae989d6f6be48e2c1af1309bad
Sanit Chhonker <141418215+Kratos956@users.noreply.github.com>
2026-04-20 15:54:58 +0530

    Time: 4 ms (21.49%), Space: 46.5 MB (57.65%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
@@ -1,30 +1,19 @@
 class Solution {
     boolean isSubtree=false;
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
-        if(root==null && subRoot==null){
-            return true;
-        }
-        if(root==null || subRoot==null) return false;
-        Preorder(root, subRoot);
-        return isSubtree;
+        if (root == null) return false;
+        if (isSameTree(root, subRoot)) return true;
+        return isSubtree(root.left, subRoot) ||
+               isSubtree(root.right, subRoot);
     }
-    void Preorder(TreeNode root,TreeNode subRoot) {
-        if(root==null) return;
-        if(root.val==subRoot.val && sameTree(root,subRoot)){
-            isSubtree=true;
-        }
-        if(!isSubtree){
-            Preorder(root.left, subRoot);
-            Preorder(root.right, subRoot);
-        }
-    }
+    
 
-    boolean sameTree(TreeNode p,TreeNode q){
+    boolean isSameTree(TreeNode p,TreeNode q){
         if(p==null && q==null) return true;
         if(p==null || q==null) return false;
         if(p.val!=q.val) return false;
-        boolean leftSubtree=sameTree(p.left,q.left);
-        boolean rightSubtree=sameTree(p.right,q.right);
+        boolean leftSubtree=isSameTree(p.left,q.left);
+        boolean rightSubtree=isSameTree(p.right,q.right);
         return leftSubtree && rightSubtree;
     }
 }
