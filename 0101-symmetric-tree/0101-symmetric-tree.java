18aa138106af716f0dbe73cf196f8e1630d3f906
Sanit Chhonker <141418215+Kratos956@users.noreply.github.com>
2026-04-20 16:04:00 +0530

    Time: 0 ms (100%), Space: 43.5 MB (56.43%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -1,22 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public boolean isSymmetric(TreeNode root) {
-        Queue<TreeNode> q=new LinkedList<>();
         
-        q.add(root.left);
-        q.add(root.right);
-        while(!q.isEmpty()){
-            TreeNode u1=q.poll();
-            TreeNode u2=q.poll();
-            if (u1 == null && u2 == null) continue;
-            if (u1 == null || u2 == null) return false;
-            if (u1.val != u2.val) return false;
-            
-            q.offer(u1.left);
-            q.offer(u2.right);
-            q.offer(u1.right);
-            q.offer(u2.left);
-        }
-        return true;
+        return generate(root.left, root.right);
     }
+    boolean generate(TreeNode L, TreeNode R) {
+        if (L==null && R==null) return true;
+        if (L==null || R==null) return false;
+        if(L.val!=R.val) return false;
+        boolean left = generate(L.left, R.right);
+        boolean right = generate(L.right, R.left);
+        return left && right;
 
+    }
 }
