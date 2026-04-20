8b59f851a8c64f76f5502b7740f73cdb44c583d0
Sanit Chhonker <141418215+Kratos956@users.noreply.github.com>
2026-04-20 16:33:42 +0530

    Time: 0 ms (100%), Space: 42.9 MB (96.75%) - LeetHub

--- 0951-flip-equivalent-binary-trees/0951-flip-equivalent-binary-trees.java
+++ 0951-flip-equivalent-binary-trees/0951-flip-equivalent-binary-trees.java
@@ -14,14 +14,17 @@
  * }
  */
 class Solution {
-    public boolean flipEquiv(TreeNode root1, TreeNode root2) {
-        if(root1==null && root2==null) return true;
-        if(root1==null ||root2==null) return false;
-        if(root1.val!=root2.val) return false;
-        boolean LL=flipEquiv(root1.left,root2.left);
-        boolean LR=flipEquiv(root1.left,root2.right);
-        boolean RL=flipEquiv(root1.right,root2.left);
-        boolean RR=flipEquiv(root1.right,root2.right);
-        return (LL&&RR)||(LR&&RL);
+    public boolean flipEquiv(TreeNode p, TreeNode q) {
+        if(p==null && q==null) return true;
+        if(p==null || q==null) return false;
+        if(p.val!=q.val) return false;
+
+        boolean left=flipEquiv(p.left,q.left);
+        boolean right=flipEquiv(p.right,q.right);
+
+        boolean flipLeft=flipEquiv(p.left,q.right);
+        boolean flipRight=flipEquiv(p.right,q.left);
+
+        return (left && right) || (flipLeft && flipRight);
     }
 }
