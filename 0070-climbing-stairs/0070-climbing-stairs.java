9a17447119794d4ca4e912ee71901e8c5066a115
Sanit Chhonker <141418215+Kratos956@users.noreply.github.com>
2026-05-27 01:50:54 +0530

    Time: 0 ms (100%), Space: 42.3 MB (16.57%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -10,16 +10,4 @@ class Solution {
         return dp[0];
 
     }
-    int find(int x,int n,int[] dp){
-        if(x==n){
-            return 1;
-        }
-        if(x>n) return 0;
-
-        if(dp[x]!=-1){
-            return dp[x];
-        }
-        dp[x]=find(x+1,n,dp)+find(x+2,n,dp);
-        return dp[x];
-    }
 }
