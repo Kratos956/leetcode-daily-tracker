69266b968e187c2eedb87f190cb674ddd6a3768f
Sanit Chhonker <141418215+Kratos956@users.noreply.github.com>
2026-05-27 01:08:38 +0530

    Time: 0 ms (100%), Space: 42.1 MB (52.76%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int climbStairs(int n) {
+        int[] dp=new int[n+1];
+        Arrays.fill(dp,-1);
+        dp[0]=1;
+        dp[1]=1;
+        return find(n,dp);
+
+    }
+    int find(int n,int[] dp){
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+        dp[n]=find(n-1,dp)+find(n-2,dp);
+        return dp[n];
+    }
+}
