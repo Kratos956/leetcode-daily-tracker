2cbb7799559dbda0c3487c96fc0e36a0140343ce
Sanit Chhonker <141418215+Kratos956@users.noreply.github.com>
2026-05-27 15:20:12 +0530

    Time: 0 ms (100%), Space: 42 MB (52.76%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,23 +1,15 @@
 class Solution {
     public int climbStairs(int n) {
-        int[] dp=new int[n+2];
-        dp[n]=1;
-        dp[n+1]=0;
+        int last=0;
+        int secondLast=1;
+        int result=1;
 
         for(int i=n-1;i>=0;i--){
-            dp[i]=dp[i+1]+dp[i+2];
+            result=last+secondLast;
+            last=secondLast;
+            secondLast=result;
         }
-        return dp[0];
+        return result;
 
-    }
-    int find(int x,int n,int[] dp){
-        if(x==n)return 1;
-        if(x>n)return 0;
-
-        //Found in dp arr
-        if(dp[x]!=-1) return dp[x];
-
-        dp[x]=find(x+1,n,dp)+find(x+2,n,dp);
-        return dp[x];
     }
 }
