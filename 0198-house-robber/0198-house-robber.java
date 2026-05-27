b3b31f36f79181617d4abd133315ec260fd67732
Sanit Chhonker <141418215+Kratos956@users.noreply.github.com>
2026-05-28 00:43:42 +0530

    Time: 0 ms (100%), Space: 42.8 MB (54.7%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,10 +1,9 @@
 class Solution {
     public int rob(int[] nums) {
-        int n=nums.length;
         int first=0;
         int second=0;
         int result=0;
-        for(int i=n-1;i>=0;i--){
+        for(int i=nums.length-1;i>=0;i--){
             result=Math.max(nums[i]+second,first);
             second=first;
             first=result;
