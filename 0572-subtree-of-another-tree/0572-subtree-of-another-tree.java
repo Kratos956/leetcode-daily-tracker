287beec14a2850141663203bf041bee753291341
Sanit Chhonker <141418215+Kratos956@users.noreply.github.com>
2026-05-30 17:11:03 +0530

    Time: 4 ms (20.3%), Space: 46.6 MB (32.81%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
@@ -1,23 +1,38 @@
 class Solution {
-
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
-        if (subRoot == null) return true;
-        if (root == null) return false;
 
-        if (root.val == subRoot.val && sameTree(root, subRoot)) {
-            return true;
+        Queue<TreeNode> queue=new LinkedList<>();
+        queue.offer(root);
+        while(!queue.isEmpty()){
+            TreeNode node=queue.poll();
+            if(node.val==subRoot.val && isSameTree(node, subRoot)){
+                return true;
+            }
+            if(node.left!=null){
+                queue.offer(node.left);
+            }
+            if(node.right!=null){
+                queue.offer(node.right);
+            }
         }
-
-        return isSubtree(root.left, subRoot) ||
-               isSubtree(root.right, subRoot);
+        return false;
     }
-
-    boolean sameTree(TreeNode p, TreeNode q) {
-        if (p == null && q == null) return true;
-        if (p == null || q == null) return false;
-        if (p.val != q.val) return false;
-
-        return sameTree(p.left, q.left) &&
-               sameTree(p.right, q.right);
+//    void inorder(TreeNode root,TreeNode subroot,boolean isSubtree){
+//        if(root==null) return;
+//        if(root.val==subroot.val && isSameTree(root,subroot)){
+//            isSubtree=true;
+//        }
+//        if(!isSubtree){
+//            inorder(root.left,subroot,isSubtree);
+//            inorder(root.right,subroot,isSubtree);
+//        }
+//    }
+    boolean isSameTree(TreeNode r1,TreeNode r2){
+        if(r1==null && r2==null) return true;
+        if(r1==null || r2==null) return false;
+        if(r1.val!=r2.val) return false;
+        boolean l=isSameTree(r1.left,r2.left);
+        boolean r=isSameTree(r1.right,r2.right);
+        return l && r;
     }
 }
