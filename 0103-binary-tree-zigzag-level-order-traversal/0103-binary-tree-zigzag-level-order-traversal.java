d0af8074e4a6952e5b939418639dafc100da2717
Sanit Chhonker <141418215+Kratos956@users.noreply.github.com>
2026-05-30 15:59:47 +0530

    Time: 1 ms (78.98%), Space: 43.4 MB (95.11%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
@@ -1,42 +1,32 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        List<List<Integer>> res=new ArrayList<>();
-        if(root==null) return res;
+
         Queue<TreeNode> q=new LinkedList<>();
         q.add(root);
-        int zigzag=1;
+        
+        List<List<Integer>> res=new ArrayList<>();
+        if(root==null) return res;
+
+        int change=0;
         while(!q.isEmpty()){
-            int size=q.size();
+            int n=q.size();
             List<Integer> temp=new ArrayList<>();
-            for(int i=0;i<size;i++){
-                TreeNode node=q.poll();
-                if(zigzag==0){
-                    temp.addFirst(node.val);
+            for(int i=0;i<n;i++){
+                TreeNode curr=q.poll();
+                
+                if(change==0){
+                    temp.add(curr.val);
                 }
-                else if(zigzag==1){
-                    temp.add(node.val);
+                else{
+                    temp.addFirst(curr.val);
                 }
-                if(node.left!=null) q.offer(node.left);
-                if(node.right!=null) q.offer(node.right);
+
+                if(curr.left!=null) q.add(curr.left);
+                if(curr.right!=null) q.add(curr.right);
             }
-            res.add(temp);
-            zigzag=1-zigzag;
-           
+
+            res.add(new ArrayList<>(temp));
+            change=1-change;
         }
         return res;
     }
