0e9c31dd32b0587758f94a0867984629ae52623d
Sanit Chhonker <141418215+Kratos956@users.noreply.github.com>
2026-05-31 01:34:12 +0530

    Time: 9 ms (65.34%), Space: 43 MB (82.75%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,36 +1,17 @@
 class Solution {
     public int change(int amount, int[] coins) {
         int n=coins.length;
-        int[][] dp=new int[n+1][amount+1];
-        
-        for(int i=0;i<dp.length;i++){
-            dp[i][0] =1;
-        }
+        int[] dp=new int[amount+1];
+
+        dp[0]=1;
         for(int i=1;i<=n;i++){
             for(int j=1;j<=amount;j++){
-                if(coins[i-1]>j){
-                    dp[i][j]=dp[i-1][j];
+                if(!(coins[i-1] >j)){
+                    dp[j]=dp[j-coins[i-1]]+dp[j];
                 }
-                else{
-                    dp[i][j]=dp[i][j-coins[i-1]]+dp[i-1][j];
-                }
+                
             }
         }
-        return dp[n][amount];
-    }
-    int find(int index,int amount,int[] coins,int[][] dp){
-        if(amount==0) return 1;
-        if(index==0) return 0;
-
-        if(dp[index][amount]!=-1){
-            return dp[index][amount];
-        }
-
-        if(coins[index-1]>amount){
-            return dp[index][amount]=find(index-1,amount,coins,dp);
-        }
-        else{
-            return dp[index][amount]=find(index,amount-coins[index-1],coins,dp)+find(index-1,amount,coins,dp);
-        }
+        return dp[amount];
     }
 }
