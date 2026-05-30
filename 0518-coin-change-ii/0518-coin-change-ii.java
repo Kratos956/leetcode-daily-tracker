9a7a393eb78b81e5587f62dec886cdce8caf4acc
Sanit Chhonker <141418215+Kratos956@users.noreply.github.com>
2026-05-31 00:03:41 +0530

    Time: 11 ms (59.26%), Space: 56.7 MB (43.02%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,22 +1,36 @@
 class Solution {
     public int change(int amount, int[] coins) {
-        int[][] dp = new int[coins.length][amount+1];
-
-        for(int i=0;i<coins.length;i++){
-        Arrays.fill(dp[i], -1);
+        int n=coins.length;
+        int[][] dp=new int[n+1][amount+1];
+        
+        for(int i=0;i<dp.length;i++){
+            dp[i][0] =1;
         }
-        return generate(0,amount,coins,dp);
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=amount;j++){
+                if(coins[i-1]>j){
+                    dp[i][j]=dp[i-1][j];
+                }
+                else{
+                    dp[i][j]=dp[i][j-coins[i-1]]+dp[i-1][j];
+                }
+            }
+        }
+        return dp[n][amount];
     }
-    int generate(int index,int amount,int[] coins,int[][] dp){
-
+    int find(int index,int amount,int[] coins,int[][] dp){
         if(amount==0) return 1;
+        if(index==0) return 0;
 
-        if(amount<0) return 0;
+        if(dp[index][amount]!=-1){
+            return dp[index][amount];
+        }
 
-        if(index>=coins.length) return 0;
-
-        if(dp[index][amount] != -1) return dp[index][amount];
-
-        return dp[index][amount]=generate(index, amount-coins[index], coins, dp)+generate(index+1, amount, coins, dp);
+        if(coins[index-1]>amount){
+            return dp[index][amount]=find(index-1,amount,coins,dp);
+        }
+        else{
+            return dp[index][amount]=find(index,amount-coins[index-1],coins,dp)+find(index-1,amount,coins,dp);
+        }
     }
 }
