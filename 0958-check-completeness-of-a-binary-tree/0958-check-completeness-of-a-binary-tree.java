4a24a9b17102bc1885e88eaf71ed1f0c324852ff
Sanit Chhonker <141418215+Kratos956@users.noreply.github.com>
2026-06-02 16:59:12 +0530

    Time: 1 ms (84.52%), Space: 44.1 MB (22.43%) - LeetHub

--- 0958-check-completeness-of-a-binary-tree/0958-check-completeness-of-a-binary-tree.java
+++ 0958-check-completeness-of-a-binary-tree/0958-check-completeness-of-a-binary-tree.java
@@ -1,34 +1,21 @@
 class Solution {
     public boolean isCompleteTree(TreeNode root) {
-        List<Integer> temp=new ArrayList<>();
+        boolean nullSeen=false;
 
         Queue<TreeNode> q=new LinkedList<>();
         q.add(root);
         while(!q.isEmpty()){
-            
-            int n=q.size();
-            for(int i=0;i<n;i++) {
-                TreeNode u = q.poll();
-                temp.add(u.val);
-                if (u.val == 0) continue;
-
-                if (u.left == null) q.add(new TreeNode(0));
-                if (u.left != null) q.add(u.left);
-                if (u.right == null) q.add(new TreeNode(0));
-                if (u.right != null) q.add(u.right);
-
+            TreeNode node=q.poll();
+            if(node==null){
+                nullSeen=true;
+            } else  {
+                if(nullSeen){
+                    return false;
+                }
+                q.add(node.left);
+                q.add(node.right);
             }
-        }
-        System.out.println(temp);
 
-        
-        boolean nowZero=true;
-        for(int i=temp.size()-1;i>=0;i--){
-            if(temp.get(i)==0 && !nowZero){
-                return false;
-            } else if (temp.get(i)!=0) {
-                nowZero=false;
-            }
         }
         return true;
 
