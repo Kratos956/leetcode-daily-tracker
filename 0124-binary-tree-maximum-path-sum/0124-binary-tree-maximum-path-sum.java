f04954c3055cfe5eba8a368d7ac68ee46fc7be0d
Sanit Chhonker <141418215+Kratos956@users.noreply.github.com>
2026-06-03 18:31:17 +0530

    Time: 0 ms (100%), Space: 46.9 MB (19.23%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.java
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.java
@@ -14,23 +14,19 @@
  * }
  */
 class Solution {
-    int res=Integer.MIN_VALUE;
     public int maxPathSum(TreeNode root) {
-        gen(root);
-        return res;
+        int[] res=new int[1];
+        res[0]=Integer.MIN_VALUE;
+        gen(root,res);
+        return res[0];
     }
-    int gen(TreeNode root){
+    int gen(TreeNode root,int[] res){
         if(root==null) return 0;
 
-        int left=gen(root.left);
-        int right=gen(root.right);
-        if(left<0){
-            left=0;
-        }
-        if(right<0){
-            right=0;
-        }
-        res=Math.max(res,Math.max(root.val,Math.max(left+right+root.val,Math.max(left+root.val,right+root.val))));
+        int left=gen(root.left,res);
+        int right=gen(root.right,res);
+        
+        res[0]=Math.max(res[0],Math.max(root.val,Math.max(left+right+root.val,Math.max(left+root.val,right+root.val))));
 
         return Math.max(root.val,Math.max(left+root.val,right+root.val));
     }
