38508228807ccc4aa1d8196ba5498e048ed85bb8
Sanit Chhonker <141418215+Kratos956@users.noreply.github.com>
2026-06-05 19:00:26 +0530

    Time: 0 ms (100%), Space: 44.3 MB (14.85%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
@@ -1,47 +1,50 @@
-class Pair{
-    TreeNode node;
+class Pair {
+    TreeNode head;
     TreeNode tail;
-    Pair(TreeNode node, TreeNode tail) {
-        this.node = node;
+
+    Pair(TreeNode head, TreeNode tail) {
+        this.head = head;
         this.tail = tail;
     }
 }
+
 class Solution {
+
     public void flatten(TreeNode root) {
-        preorder(root);
+        dfs(root);
     }
-    Pair preorder(TreeNode root){
-        if(root==null){
-            return new Pair(null,null);
-        }
-        
 
-        Pair left=preorder(root.left);
-        Pair right=preorder(root.right);
-
-        if(left.node==null && right.node==null){
-            root.left=null;
-            return new Pair(root,root);
-        }
-        else if(left.node==null && right.node!=null){
-            root.right=right.node;
-            root.left=null;
-            return new Pair(root,right.tail);
-        }
-        else if(left.node!=null && right.node==null){
-            root.right=left.node;
-            root.left=null;
-            return new Pair(root,left.tail);
-        }
-        
-        else{
-            root.right=left.node;
-            left.tail.right=right.node;
-            root.left=null;
-            return new Pair(root,right.tail);
+    Pair dfs(TreeNode root) {
+        if (root == null) {
+            return new Pair(null, null);
         }
 
+        Pair left = dfs(root.left);
+        Pair right = dfs(root.right);
 
+        root.left = null;
 
+        // Leaf
+        if (left.head == null && right.head == null) {
+            return new Pair(root, root);
+        }
+
+        // Only right subtree
+        if (left.head == null) {
+            root.right = right.head;
+            return new Pair(root, right.tail);
+        }
+
+        // Only left subtree
+        if (right.head == null) {
+            root.right = left.head;
+            return new Pair(root, left.tail);
+        }
+
+        // Both exist
+        root.right = left.head;
+        left.tail.right = right.head;
+
+        return new Pair(root, right.tail);
     }
 }
