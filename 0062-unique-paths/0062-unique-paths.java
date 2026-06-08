a18df7d12bf7412dfcb1702909cbaf2347585c9c
Sanit Chhonker <141418215+Kratos956@users.noreply.github.com>
2026-06-09 00:28:45 +0530

    Time: 0 ms (100%), Space: 41.7 MB (97.4%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -4,24 +4,23 @@ class Solution {
     }
     public int uniquePaths(int m, int n) {
         int[][] dp=new int[m+1][n+1];
-        for (int[] ints : dp) {
-            Arrays.fill(ints, -1);
+        for(int i=0;i<dp[0].length;i++){
+            dp[m][i]=1;
+        }
+        for(int i=0;i<dp.length;i++){
+            dp[i][n]=1;
         }
 
-        generate(1,1,m,n,dp);
+        for(int i=m-1;i>0;i--){
+            for(int j=n-1;j>0;j--){
+                if(i==m && j==n) dp[i][j]=1;
+                else{
+                    dp[i][j]=dp[i+1][j]+dp[i][j+1];
+                }
+            }
+
+        }
+
         return dp[1][1];
-    }
-    int generate(int i,int j,int m,int n,int[][] dp){
-        if(!Valid(i,j,m,n)) return 0;
-        if(i==m && j==n){
-            return dp[i][j]=1;
-        }
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-        int bottom=generate(i+1,j,m,n,dp);
-        int right=generate(i,j+1,m,n,dp);
-
-        return dp[i][j]=bottom+right;
     }
 }
