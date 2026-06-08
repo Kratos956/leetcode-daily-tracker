a62d31cb0e20c02f4ea1d6d4f7b395dbbf5db8d5
Sanit Chhonker <141418215+Kratos956@users.noreply.github.com>
2026-06-09 00:46:31 +0530

    Time: 0 ms (100%), Space: 42 MB (63.95%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,22 +1,12 @@
 class Solution {
-    boolean Valid(int i,int j,int m,int n){
-        return i>=1 && i<=m && j>=1 && j<=n;
-    }
     public int uniquePaths(int m, int n) {
         int[][] dp=new int[m+1][n+1];
-        for(int i=0;i<dp[0].length;i++){
-            dp[m][i]=1;
-        }
-        for(int i=0;i<dp.length;i++){
-            dp[i][n]=1;
-        }
+        for(int i=0;i<dp[0].length;i++) dp[m][i]=1;
+        for(int i=0;i<dp.length;i++) dp[i][n]=1;
 
         for(int i=m-1;i>0;i--){
             for(int j=n-1;j>0;j--){
-                if(i==m && j==n) dp[i][j]=1;
-                else{
-                    dp[i][j]=dp[i+1][j]+dp[i][j+1];
-                }
+                dp[i][j]=dp[i+1][j]+dp[i][j+1];
             }
 
         }
