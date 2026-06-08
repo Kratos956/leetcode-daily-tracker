2da8515f4828c5800fb6c5db44e746f8d717927f
Sanit Chhonker <141418215+Kratos956@users.noreply.github.com>
2026-06-09 00:51:06 +0530

    Time: 0 ms (100%), Space: 42.3 MB (12.09%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,15 +1,15 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        int[][] dp=new int[m+1][n+1];
-        for(int i=0;i<dp.length;i++) Arrays.fill(dp[i],1);
+        int[] dp=new int[n+1];
+        Arrays.fill(dp,1);
 
         for(int i=m-1;i>0;i--){
             for(int j=n-1;j>0;j--){
-                dp[i][j]=dp[i+1][j]+dp[i][j+1];
+                dp[j]=dp[j]+dp[j+1];
             }
 
         }
 
-        return dp[1][1];
+        return dp[1];
     }
 }
