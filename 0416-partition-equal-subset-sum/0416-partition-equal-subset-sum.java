d5dcb84c9701eac41cdab6d12d995d78c4c42230
Sanit Chhonker <141418215+Kratos956@users.noreply.github.com>
2026-06-27 00:15:16 +0530

    Time: 21 ms (93.34%), Space: 75.4 MB (18.78%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,24 +1,39 @@
 class Solution {
     public boolean canPartition(int[] nums) {
+        int n=nums.length;
         int total=0;
-        for(int i=0;i<nums.length;i++){
-            total+=nums[i];
-        }
+        for(int i=0;i<nums.length;i++) total+=nums[i];
         if(total%2!=0) return false;
         
-        Boolean[][] dp=new Boolean[nums.length+1][total/2+1];
+        Boolean[][] dp=new Boolean[n+1][total/2+1];
+        // Arrays.fill(dp[n],false);          //LAST ROW 
+        // for(int i=0;i<dp.length;i++){
+        //     dp[i][total/2]=true;           //LAST COL
+        // }
+        // for(int i=n-1;i>-1;i--){
+        //     for(int j=total/2-1;j>-1;j--){
+        //         if((i+nums[i])*2>j){
+        //             dp[i][j]=dp[i+1][j];
+        //         }
+        //         else{
+        //             dp[i][j]=dp[i+1][j+nums[i]] || dp[i+1][j];
+        //         }
+        //     }
+        // }
+
+
 
         return generate(0,0,total,nums,dp);
     }
     boolean generate(int index,int sum,int total,int[] nums,Boolean[][] dp){
         if(sum+sum==total) return true;
         if(index==nums.length) return false;
-        if((2*sum)>total){
-            return false;
-        }
 
         if(dp[index][sum]!=null){
             return dp[index][sum];
+        }
+        if((sum+nums[index])*2>total){
+            return generate(index+1,sum,total,nums,dp);
         }
 
         return dp[index][sum]=
