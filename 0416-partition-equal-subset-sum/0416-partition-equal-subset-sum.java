de1a08470da133727655ca36c86a62d88f12dab1
Sanit Chhonker <141418215+Kratos956@users.noreply.github.com>
2026-06-27 00:16:37 +0530

    Time: 86 ms (25.53%), Space: 75.4 MB (19.62%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -6,24 +6,24 @@ class Solution {
         if(total%2!=0) return false;
         
         Boolean[][] dp=new Boolean[n+1][total/2+1];
-        // Arrays.fill(dp[n],false);          //LAST ROW 
-        // for(int i=0;i<dp.length;i++){
-        //     dp[i][total/2]=true;           //LAST COL
-        // }
-        // for(int i=n-1;i>-1;i--){
-        //     for(int j=total/2-1;j>-1;j--){
-        //         if((i+nums[i])*2>j){
-        //             dp[i][j]=dp[i+1][j];
-        //         }
-        //         else{
-        //             dp[i][j]=dp[i+1][j+nums[i]] || dp[i+1][j];
-        //         }
-        //     }
-        // }
+        Arrays.fill(dp[n],false);          //LAST ROW 
+        for(int i=0;i<dp.length;i++){
+            dp[i][total/2]=true;           //LAST COL
+        }
+        for(int i=n-1;i>-1;i--){
+            for(int j=total/2-1;j>-1;j--){
+                if((j+nums[i])*2>total){
+                    dp[i][j]=dp[i+1][j];
+                }
+                else{
+                    dp[i][j]=dp[i+1][j+nums[i]] || dp[i+1][j];
+                }
+            }
+        }
 
 
 
-        return generate(0,0,total,nums,dp);
+        return dp[0][0];
     }
     boolean generate(int index,int sum,int total,int[] nums,Boolean[][] dp){
         if(sum+sum==total) return true;
