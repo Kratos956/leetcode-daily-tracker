5958c5cb55ac5a8e7be1314799eca109c84c8f87
Sanit Chhonker <141418215+Kratos956@users.noreply.github.com>
2026-06-26 19:49:09 +0530

    Time: 50 ms (5.77%), Space: 57.4 MB (10.75%) - LeetHub

--- 0886-possible-bipartition/0886-possible-bipartition.java
+++ 0886-possible-bipartition/0886-possible-bipartition.java
@@ -11,7 +11,7 @@ class Solution {
         System.out.println(Arrays.deepToString(adj.toArray()));
         int[] colour=new int[n];
         Arrays.fill(colour,-1);
-        for(int i=0;i<n-1;i++){
+        for(int i=0;i<n;i++){
             if(colour[i]==-1 && !DFS(i,-1,colour,adj)){
                 return false;
             }
