747e8d04d43fa77f9622635e78f8f133e3031274
Sanit Chhonker <141418215+Kratos956@users.noreply.github.com>
2026-07-04 15:10:36 +0530

    Time: 21 ms (78.44%), Space: 108.5 MB (95.73%) - LeetHub

--- 2050-parallel-courses-iii/2050-parallel-courses-iii.java
+++ 2050-parallel-courses-iii/2050-parallel-courses-iii.java
@@ -1,54 +1,32 @@
 class Solution {
     public int minimumTime(int n, int[][] relations, int[] time) {
-
-        List<List<Integer>> adj = new ArrayList<>();
-        for(int i = 0; i < n; i++) adj.add(new ArrayList<>());
-
-        int[] degree = new int[n];
-
-        for(int[] edge : relations){
-            int u = edge[0] - 1;
-            int v = edge[1] - 1;
-
+        List<List<Integer>> adj=new ArrayList<>();
+        for(int i=0;i<n;i++) adj.add(new ArrayList<>());
+        int[] degree=new int[n];
+        for(int[] relation:relations){
+            int u=relation[0]-1;
+            int v=relation[1]-1;
             adj.get(u).add(v);
             degree[v]++;
         }
-
-        Queue<Integer> q = new LinkedList<>();
-
-        // earliest time when a course can START
-        int[] startTime = new int[n];
-
-        for(int i = 0; i < n; i++){
-            if(degree[i] == 0){
-                q.offer(i);
-            }
+        int[] EST=new int[n];
+        Queue<Integer> q=new LinkedList<>();
+        for(int i=0;i<n;i++){
+            if(degree[i]==0) q.add(i);
         }
-
         while(!q.isEmpty()){
-
-            int u = q.poll();
-
-            for(int next : adj.get(u)){
-
-                // next can start only after u finishes
-                startTime[next] = Math.max(startTime[next],
-                                           startTime[u] + time[u]);
-
-                degree[next]--;
-
-                if(degree[next] == 0){
-                    q.offer(next);
-                }
+            int u=q.poll();
+            for(int ele:adj.get(u)){
+                degree[ele]--;
+                EST[ele]=Math.max(EST[ele],EST[u]+time[u]);
+                if(degree[ele]==0) q.add(ele);
             }
         }
-
-        int answer = 0;
-
-        for(int i = 0; i < n; i++){
-            answer = Math.max(answer, startTime[i] + time[i]);
+        int ans=Integer.MIN_VALUE;
+        for(int i=0;i<EST.length;i++){
+            ans=Math.max(ans,EST[i]+time[i]);
         }
-
-        return answer;
+        return ans;
+        
     }
 }
