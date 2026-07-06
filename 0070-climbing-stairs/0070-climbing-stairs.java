b2870d85de228c441891d675efd81430c91313bb
Sanit Chhonker <141418215+Kratos956@users.noreply.github.com>
2026-07-06 14:47:03 +0530

    Time: 0 ms (100%), Space: 42.1 MB (54.17%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,12 +1,17 @@
-class Solution {
-    public int climbStairs(int n) {
-        int first=1;
-        int second=1;
-        for(int i=2;i<n+1;i++){
-            int third=first+second;
-            first=second;
-            second=third;
-        }
-        return second;
+class Solution 
+{
+    public int climbStairs(int n) 
+    {
+        if(n<=2)
+        return n;
+        int a=1,b=2,c;
+        for(int i=3;i<=n;i++)
+        {
+            c=a+b;
+            a=b;
+            b=c;
+        }  
+        return b;
+
     }
 }
