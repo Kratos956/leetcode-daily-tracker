28efae8bf1c5b3e076b14caf331aa0413e90b444
Sanit Chhonker <141418215+Kratos956@users.noreply.github.com>
2026-07-06 14:22:23 +0530

    Time: 0 ms (100%), Space: 42 MB (51.92%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -3,18 +3,13 @@ class Solution {
         if(n==0 || n==1) return n;
         int a=0;
         int b=1;
-        int c=0;
+        
         for(int i=2;i<n+1;i++){
-            c=a+b;
+            int c=a+b;
             a=b;
             b=c;
         }
 
-        return c;
-    }
-    int dpSol(int n,int[] dp){
-        
-        if(dp[n]!=-1) return dp[n];
-        return dp[n]=fib(n-1)+fib(n-2);
+        return b;
     }
 }
