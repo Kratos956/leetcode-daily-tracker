d3908f0d3d1b30eb1c184bac0f530f8fd7eaa24d
Sanit Chhonker <141418215+Kratos956@users.noreply.github.com>
2026-07-07 23:46:55 +0530

    Time: 0 ms (100%), Space: 44.8 MB (58.87%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -1,27 +1,16 @@
 class Solution {
     public int minCostClimbingStairs(int[] cost) {
-        int n=cost.length;
-        int[] dp=new int[n+1];
+        int n = cost.length;
 
-        dp[n]  = 0;
-        dp[n-1]= cost[n-1];
+        int Last = 0;
+        int secondLast = cost[n - 1];
 
-        for(int i=n-2;i>=0;i--){
-            dp[i]=Math.min(cost[i]+dp[i+1],cost[i]+dp[i+2]);
+        for (int i = n - 2; i >= 0; i--) {
+            int third = Math.min(cost[i] + secondLast, cost[i] + Last);
+            Last = secondLast;
+            secondLast = third;
         }
 
-        return Math.min(dp[0],dp[1]);
-    }
-    int find(int index,int n,int[] cost,int[] dp){
-        if(index==n) return 0;
-
-        if(index==n-1){
-            return dp[index] = cost[index]+find(index+1,n,cost,dp);
-        }
-        if(dp[index]!=-1) return dp[index];
-
-        return 
-        dp[index] = Math.min(cost[index]+find(index+1,n,cost,dp),
-        cost[index]+find(index+2,n,cost,dp));
+        return Math.min(Last, secondLast);
     }
 }
