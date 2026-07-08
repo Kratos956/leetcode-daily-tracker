76c5fcc614199328d441e6b26e0cc654afc5964d
Sanit Chhonker <141418215+Kratos956@users.noreply.github.com>
2026-07-09 00:11:05 +0530

    Time: 0 ms (100%), Space: 43 MB (22.35%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,29 +1,32 @@
 class Solution {
     public int rob(int[] nums) {
-        int n=nums.length;
-        if(n==1) return nums[0];
-        if(n==2) return Math.max(nums[0],nums[1]);
-        int[] dp1=new int[3];
-        int[] dp2=new int[3];
-        dp1[1]=nums[n-2];
-        dp1[0]=Math.max(nums[n-3],nums[n-2]);
-        for(int i=n-4;i>=0;i--){
+        int n = nums.length;
 
-            dp1[2]=dp1[1];
-            dp1[1]=dp1[0];
+        if (n == 1) return nums[0];
+        if (n == 2) return Math.max(nums[0], nums[1]);
 
-            dp1[0]=Math.max(nums[i]+dp1[2],dp1[1]);
+        // Case 1: Rob houses [0 ... n-2]
+        int curr1 = Math.max(nums[n - 3], nums[n - 2]);
+        int next1 = nums[n - 2];
+        int nextNext1;
+
+        for (int i = n - 4; i >= 0; i--) {
+            nextNext1 = next1;
+            next1 = curr1;
+            curr1 = Math.max(nums[i] + nextNext1, next1);
         }
 
-        dp2[1]=nums[n-1];
-        dp2[0]=Math.max(nums[n-1],nums[n-2]);
-        for(int j=n-3;j>0;j--){
-            dp2[2]=dp2[1];
-            dp2[1]=dp2[0];
-            dp2[0]=Math.max(nums[j]+dp2[2],dp2[1]);
+        // Case 2: Rob houses [1 ... n-1]
+        int curr2 = Math.max(nums[n - 2], nums[n - 1]);
+        int next2 = nums[n - 1];
+        int nextNext2;
+
+        for (int i = n - 3; i > 0; i--) {
+            nextNext2 = next2;
+            next2 = curr2;
+            curr2 = Math.max(nums[i] + nextNext2, next2);
         }
 
-
-        return Math.max(dp1[0],dp2[0]);
+        return Math.max(curr1, curr2);
     }
 }
