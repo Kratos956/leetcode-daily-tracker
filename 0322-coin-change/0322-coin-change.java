aa8d00934f14ae533dde3f61b3894c8c16225500
Sanit Chhonker <141418215+Kratos956@users.noreply.github.com>
2026-07-18 20:04:20 +0530

    Time: 17 ms (48.48%), Space: 46.6 MB (39.59%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,36 +1,31 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        int n=coins.length;
-        int[][] dp=new int[n+1][amount+1];
 
-        Arrays.fill(dp[0],10001);
-        for(int i=0;i<dp.length;i++){
-            dp[i][0]=0;
-        }
-        for(int i=1;i<dp.length;i++){
-            for(int j=1;j<dp[i].length;j++){
-                if(j<coins[i-1]) dp[i][j]=dp[i-1][j];
-                else{
-                    dp[i][j]=Math.min(1+dp[i][j-coins[i-1]],dp[i-1][j]);
+        int n = coins.length;
+
+        int[] prev = new int[amount + 1];
+        int[] curr = new int[amount + 1];
+        Arrays.fill(prev, 10001);
+        prev[0] = 0;
+        curr[0] = 0;
+
+        for (int i = 1; i <= n; i++) {
+
+            
+            
+
+            for (int j = 1; j <= amount; j++) {
+
+                if (j < coins[i - 1]) {
+                    curr[j] = prev[j];
+                } else {
+                    curr[j] = Math.min(1 + curr[j - coins[i - 1]], prev[j]);
                 }
             }
-        }
-        if(dp[n][amount]>=10001) return -1;
-        return dp[n][amount];
 
-    }
-    int generate(int n,int amount,int[] coins,int[][] dp){
-        if(amount==0) return 0;
-        if(n==0) return 10001;
-
-        if(dp[n][amount]!=-1) return dp[n][amount];
-
-        if(amount<coins[n-1]){
-            return dp[n][amount]=generate(n-1,amount,coins,dp);
+            prev = curr;
         }
 
-        return dp[n][amount]=Math.min(1 + generate(n,amount-coins[n-1],coins,dp),
-                generate(n-1,amount,coins,dp)
-        );
+        return prev[amount] >= 10001 ? -1 : prev[amount];
     }
 }
